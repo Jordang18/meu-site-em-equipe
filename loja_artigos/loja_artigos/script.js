4074fa267a508c8bab1eb9e15afeed6b69a12208
Jordang18 <lulu_lu18@outlook.com.br>
2024-09-16 20:55:47 -0300

    Add files via upload

---
+++ loja_artigos/loja_artigos/script.js
@@ -0,0 +1,40 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Muda o slide a cada 2 segundos
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    let slides = document.getElementsByClassName("slide");
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+let boxBuscar = document.querySelector('.buscar-box');
+let lupa = document.querySelector('.lupa-buscar');
+let btnFechar = document.querySelector('.btn-fechar');
+
+lupa.addEventListener('click', ()=> {
+    boxBuscar.classList.add('ativar')
+
+})
+
+btnFechar.addEventListener('click', ()=> {
+    boxBuscar.classList.remove('ativar')
+
+})
+
+
